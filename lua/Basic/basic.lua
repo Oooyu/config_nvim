961f3a56eca38fd2beaa2cce0fd71e7dbfec235d
ChuMeng <Montyzhao@foxmail.com>
2023-07-14 18:36:51 +0800

    verilog_detect

--- lua/Basic/basic.lua
+++ lua/Basic/basic.lua
@@ -1,11 +1,12 @@
-local opt = vim.opt 
+local opt = vim.opt
+local cmd = vim.cmd
 
 opt.number = true
 
 -- tabs & indentation
-opt.tabstop = 4 -- 2 spaces for tabs (prettier default)
-opt.shiftwidth = 4 -- 2 spaces for indent width
-opt.expandtab = true -- expand tab to spaces
+opt.tabstop = 4       -- 2 spaces for tabs (prettier default)
+opt.shiftwidth = 4    -- 2 spaces for indent width
+opt.expandtab = true  -- expand tab to spaces
 opt.autoindent = true -- copy indent from current line when starting new one
 
 -- cursor line
@@ -20,8 +21,8 @@ opt.signcolumn = "yes" -- show sign column so that text doesn't shift
 -- clipboard
 opt.clipboard:append("unnamedplus") -- use system clipboard as default register
 
-opt.ignorecase = true -- ignore case when searching
-opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
+opt.ignorecase = true               -- ignore case when searching
+opt.smartcase = true                -- if you include mixed case in your search, assumes you want case-sensitive
 
 -- split windows
 opt.splitright = true -- split vertical window to the right
@@ -34,4 +35,6 @@ opt.laststatus = 2
 function MyStatusLine()
     return "[%F]%y%r%m%*%=[Line:%l/%L,Column:%c][%p%%]"
 end
+
 opt.statusline = MyStatusLine()
+cmd(vim.cmd('autocmd BufRead,BufNewFile *.v set filetype=verilog'))
